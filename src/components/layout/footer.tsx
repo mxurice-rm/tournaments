624f61324e2dddfe97112cd6f9f09143ddb05277
mxurice-rm <reim@interaktiv.de>
2025-07-25 16:37:26 +0200

    Refactor footer structure and improve Docker Compose configuration:
    - Simplify `footer.tsx` layout by streamlining text elements.
    - Restructure `docker-compose.yml`: Adjust service order, update healthcheck interval, and improve readability with consistent formatting.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,17 +4,15 @@ const Footer = () => {
   return (
     <footer className="py-8 border-t dark:bg-primary-foreground/30 backdrop-blur-sm flex justify-center">
       <div className="flex flex-col items-center">
-          <span className="text-sm text-center">
-            © 2025 RW BERRENDORF FUßBALL. Alle Rechte vorbehalten.
-          </span>
+        <span className="text-sm text-center">
+          © 2025 RW BERRENDORF FUßBALL. Alle Rechte vorbehalten.
+        </span>
 
         <div className="flex items-center space-x-2 group text-foreground">
-          <span className="text-sm text-foreground">Entwickelt mit</span>
-          <div className="w-4 h-4 text-red-500">❤️</div>
-          <span className="text-sm">von</span>
+          <span className="text-sm text-foreground">Entwickelt von</span>
           <span className="text-transparent primary-gradient bg-clip-text font-bold text-sm">
-              Maurice R.
-            </span>
+            Maurice R.
+          </span>
           <div className="text-xs">©</div>
         </div>
       </div>
@@ -22,4 +20,4 @@ const Footer = () => {
   )
 }
 
-export default Footer;
+export default Footer
